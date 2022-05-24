4b6d189f019f61301a52ee1964125404dfa36fd6
Mau-NT <mautdhd@gmail.com>
2022-05-24 14:00:41 -0400

    When target has a tp request of same player this has been deleted

--- src/main/java/cykuta/etheriacore/commands/tpa/Tpa.java
+++ src/main/java/cykuta/etheriacore/commands/tpa/Tpa.java
@@ -30,20 +30,23 @@ public class Tpa implements CommandExecutor {
             return false;
         }
         Player target = Bukkit.getPlayer(args[0]);
-
         if (target == null){
             Chat.playerMsg(player, plugin.error_prefix +
                     plugin.lang.getString("error-no-player"));
             return false;
         }
-
         if (target == player){
             Chat.playerMsg(player, plugin.error_prefix +
                     plugin.lang.getString("error-auto-target"));
             return false;
         }
-        TpaRequest.newRequest(plugin, player, target);
+        if (TpaRequest.fetchPlayer(target) == player){
+            Chat.playerMsg(player, plugin.error_prefix +
+                    plugin.lang.getString("teleport-already-send"));
+            return false;
+        }
 
+        TpaRequest.newRequest(plugin, player, target);
         Chat.playerMsg(player, plugin.main_prefix +
                 plugin.lang.getString("teleport-send").replaceAll("%player%", target.getName()));
         Chat.playerMsg(target, plugin.main_prefix +
